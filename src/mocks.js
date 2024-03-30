c3c9aceffaa984121377046d879aa5b17113b061
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-03-30 20:27:50 +0300

    Refactor code to improve performance and readability

---
+++ src/mocks.js
@@ -0,0 +1,20 @@
+export const MOCKS_POSTS = [
+  {
+    userId: 1,
+    id: 1,
+    title: "title 1",
+    body: "body 1",
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "title 2",
+    body: "body 2",
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "title 3",
+    body: "body 3",
+  },
+];
